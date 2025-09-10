54bb31c1fbcf502ba9b5b5f5eaf313065d97dcd2
Ranjeet Singh <ranjeetsingh.saundh@gmail.com>
2025-09-10 15:32:22 +0530

    Added firebase project files

---
+++ Maahees-MyFactoryApp/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard (Dummy)</h2>
+      <p>Welcome to Maahee's MyFactoryApp Dashboard</p>
+    </div>
+  );
+}
+
+export default Dashboard;
