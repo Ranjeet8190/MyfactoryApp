b37b826ae4dd0f5a35244a1fa1400638284bfdb3
Ranjeet Singh <ranjeetsingh.saundh@gmail.com>
2025-09-10 16:57:55 +0530

    Auto update -9--02_16-57-55

--- auto_push.bat
+++ auto_push.bat
@@ -1,11 +1,17 @@
 @echo off
-cd C:\Users\ranje\Documents\MyfactoryApp
+set datetime=%date:~10,4%-%date:~4,2%-%date:~7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
+set datetime=%datetime: =0%
 
-REM Add all changes
-git add .
+echo [main] Auto update %datetime%
+
+REM Always add key project files
+git add firebase.json package.json public src build .github
+
+REM Add any other changes
+git add -A
 
 REM Commit with timestamp
-git commit -m "Auto update %date% %time%"
+git commit -m "Auto update %datetime%"
 
 REM Push to GitHub
 git push -u origin main
