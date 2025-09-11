1fdb493b45d54e376ebf7c432d21e51c41b5e18e
Ranjeet Singh <ranjeetsingh.saundh@gmail.com>
2025-09-11 15:04:31 +0530

    Auto update -9--02_15-04-31

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h2>Login (Dummy)</h2>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/UsersList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function UsersList() {
+  return (
+    <div>
+      <h2>Users List (Dummy)</h2>
+      <ul>
+        <li>Dummy User 1</li>
+        <li>Dummy User 2</li>
+        <li>Dummy User 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default UsersList;
